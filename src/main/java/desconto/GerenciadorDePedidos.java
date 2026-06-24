de73b43b2b2e1953c480fa2e55c290e4021aa9ca
Msampaioalves <miguelsampaioalves@gmai.com>
2026-06-24 14:43:13 -0300

    refactor: adicionar validações no GerenciadorDePedidos

--- src/main/java/desconto/GerenciadorDePedidos.java
+++ src/main/java/desconto/GerenciadorDePedidos.java
@@ -3,9 +3,15 @@ package desconto;
 public class GerenciadorDePedidos {
 
     public double fecharPedido(Pedido pedido, CalculadoraDesconto estrategiaDesconto) {
+        if (pedido == null) {
+            throw new IllegalArgumentException("Pedido não pode ser nulo");
+        }
+        if (estrategiaDesconto == null) {
+            throw new IllegalArgumentException("Estratégia de desconto não pode ser nula");
+        }
+
         double desconto = estrategiaDesconto.calcular(pedido);
         double valorFinal = pedido.getValorTotalBruto() - desconto;
-
 
         return Math.max(0.0, valorFinal);
     }
